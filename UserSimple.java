48954ea7021ba3e8e55de3fdb1f9fd75b56c1963
Drishti Malhotra <71998960+Drishti05@users.noreply.github.com>
2022-02-27 00:05:34 +0530

    Create UserSimple.java

---
+++ UserSimple.java
@@ -0,0 +1,13 @@
+public class UserSimple {
+    public String name;
+    private String email;
+    private int age;
+    private boolean isDeveloper;
+
+    public UserSimple(String name,String email,int age,boolean isDeveloper){
+        this.name = name;
+        this.email = email;
+        this.age = age;
+        this.isDeveloper = isDeveloper;
+    }
+}
